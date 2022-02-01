9cdfb292cba4f8b0f51b9feb726ed7ad6ec773b7
Lounderr <lounder044@gmail.com>
2022-02-01 21:17:31 +0200

    Implement Front-end Shared Layout and Favicon

---
+++ src/AssignmentManager.Web/wwwroot/js/layout.js
@@ -0,0 +1,24 @@
+﻿////window.addEventListener('DOMContentLoaded', (event) => {
+////     Header
+
+////    // Menu button
+////    const mobileBtn = document.querySelector(".menu_btn");
+////    const nav = document.querySelector(".navigation");
+////    mobileBtn.addEventListener("click", () => {
+////        nav.classList.toggle("menu_active");
+////        mobileBtn.classList.toggle("btn_active");
+////    });
+
+////    // !!! Does not work when loading a new page !!! Active page 
+////    //const navListItems = document.getElementsByClassName("nav_list__li");
+
+////    //for (const item of navListItems) {
+////    //    item.addEventListener('click', (e) => {
+////    //        for (const item of navListItems) {
+////    //            item.classList.remove("nav_list__li--active");
+////    //        }
+////    //        item.classList.add("nav_list__li--active");
+////    //    });
+////    //}
+
+////});
