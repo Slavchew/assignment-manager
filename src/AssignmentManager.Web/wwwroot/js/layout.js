c2ebdcd1d209a186f75ef73f5dabd22df52ef74a
Lounderr <lounder044@gmail.com>
2022-03-12 12:27:32 +0200

    Redirect New Users to 'Classes/Welcome'

--- src/AssignmentManager.Web/wwwroot/js/layout.js
+++ src/AssignmentManager.Web/wwwroot/js/layout.js
@@ -1,6 +1,4 @@
 ﻿window.addEventListener('DOMContentLoaded', (event) => {
-    // Header
-
     // Menu button
     const menuBtn = document.getElementsByClassName("menu_btn")[0];
     const nav = document.getElementsByClassName("nav")[0];
@@ -10,4 +8,10 @@
         cornerGraphic.classList.toggle("corner_graphic_mobile--menu_btn_active");
         menuBtn.classList.toggle("menu_btn--active");
     });
+
+    // Local storage
+    if (localStorage.getItem("notFirstTime") === null) {
+        window.location.href = '/Classes/Welcome';
+        localStorage.setItem("notFirstTime", false);
+    }
 });
